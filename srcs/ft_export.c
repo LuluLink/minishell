73eb2985bd424029a9f19add2377197c3fea12dc
user42 <paul13.correia@gmail.com>
2021-03-26 12:20:44 +0100

    change ft_export

--- srcs/ft_export.c
+++ srcs/ft_export.c
@@ -1,5 +1,98 @@
 #include "minishell.h"
 
+int remplace_env(char *str)
+{
+    int i;
+    t_elem_env *tmp;
+
+    i = 0;
+    tmp = g_all.first_env;
+    while (str[i] != '=')
+        i++;
+    while (tmp != NULL)
+    {
+        if ((ft_strncmp(tmp->env, str, i) == 0))
+        {
+            free(tmp->env);
+            tmp->env = str;
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+    return (0);
+}
+
+int count_without_quote(char *str)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '\"')
+        {
+            i++;
+            while (str[i] != '\"' && str[i] != '\0')
+            {
+                i++;
+                j++;
+            }
+            i++;
+        }
+        if (str[i] == '\'')
+        {
+            i++;
+            while (str[i] != '\'' && str[i] != '\0')
+            {
+                i++;
+                j++;
+            }
+            i++;
+        }
+        if (str[i] != '\0')
+        {
+            j++;
+            i++;
+        }
+    }
+    return (j);
+}
+
+char    *str_to_env(char *str)
+{
+    int i;
+    int j;
+    char *dest;
+
+    i = 0;
+    j = count_without_quote(str);
+    dest = malloc(sizeof(char) * (j + 1));
+    dest[j] = '\0';
+    j = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '\"')
+        {
+            i++;
+            while (str[i] != '\"' && str[i] != '\0')
+                dest[j++] = str[i++];
+            i++;
+        }
+        if (str[i] == '\'')
+        {
+            i++;
+            while (str[i] != '\'' && str[i] != '\0')
+                dest[j++] = str[i++];
+            i++;
+        }
+        if (str[i] != '\0')
+            dest[j++] = str[i++];
+    }
+    return (dest);
+}
+
 int     verif_arg_export(t_elem_cmd *actual)
 {
     int i;
@@ -66,8 +159,9 @@ void    ft_export(t_elem_cmd *actual)
     {
         if ((verif_arg_export(actual) == -1))
             printf("export : %s : indentifiant invalide\n", actual->next->cmd);
-        str = ft_strdup(actual->next->cmd);
-        insertion_end_env(str);
+        str = str_to_env(actual->next->cmd);
+        if ((remplace_env(str) == 0))
+            insertion_end_env(str);
         actual = actual->next;
     }
 }
